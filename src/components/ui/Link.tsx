ddd0a5b828e82162e8231aec557de012f624c083
Arnaud Gathy <arnaud.gathy@gmail.com>
2025-03-23 19:05:49 +0100

    Fixed spell link display

--- src/components/ui/Link.tsx
+++ src/components/ui/Link.tsx
@@ -1,16 +1,21 @@
 import NextLink from "next/link";
 import { AnchorHTMLAttributes, ReactNode } from "react";
 import { LinkProps } from "next/dist/client/link";
+import { cn } from "@/lib/utils";
 
 export const Link = ({
   children,
+  className,
   ...props
 }: LinkProps &
   AnchorHTMLAttributes<HTMLAnchorElement> & { children: ReactNode }) => {
   return (
     <NextLink
       {...props}
-      className="text-muted-foreground underline hover:text-foreground"
+      className={cn(
+        "text-muted-foreground underline hover:text-foreground",
+        className,
+      )}
     >
       {children}
     </NextLink>
