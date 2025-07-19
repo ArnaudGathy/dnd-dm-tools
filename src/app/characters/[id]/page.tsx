a2e0394e25748772ea4b913805c5ef8231628d0e
Arnaud Gathy <arnaud.gathy@gmail.com>
2025-07-19 20:04:51 +0200

    Added searchParams to navigate into character sheets tabs

--- src/app/characters/[id]/page.tsx
+++ src/app/characters/[id]/page.tsx
@@ -1,27 +1,18 @@
 import { getValidCharacter } from "@/lib/utils";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { entries } from "remeda";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
 import { Card } from "@/components/ui/card";
 import Summary from "@/app/characters/[id]/(sheet)/Summary";
 import Combat from "@/app/characters/[id]/(sheet)/Combat";
 import Skills from "@/app/characters/[id]/(sheet)/Skills";
 import Inventory from "@/app/characters/[id]/(sheet)/Inventory";
 import Bio from "@/app/characters/[id]/(sheet)/Bio";
-import {
-  Backpack,
-  ChartNoAxesColumn,
-  FileText,
-  Swords,
-  User,
-  Wrench,
-  FileQuestionMark,
-  LoaderCircle,
-} from "lucide-react";
+import { LoaderCircle } from "lucide-react";
 import Settings from "@/app/characters/[id]/(sheet)/Settings";
 import Quests from "@/app/characters/[id]/(sheet)/Quests";
 import { Suspense } from "react";
+import SheetTabTrigger from "@/app/characters/[id]/(sheet)/SheetTabTrigger";
 
-enum SHEETS_TABS {
+export enum SHEETS_TABS {
   GENERAL = "general",
   COMBAT = "combat",
   SKILLS = "skills",
@@ -31,38 +22,24 @@ enum SHEETS_TABS {
   SETTINGS = "settings",
 }
 
-const tabs = {
-  [SHEETS_TABS.GENERAL]: { label: "Général", icon: FileText },
-  [SHEETS_TABS.SKILLS]: { label: "Compétences", icon: ChartNoAxesColumn },
-  [SHEETS_TABS.COMBAT]: { label: "Combat", icon: Swords },
-  [SHEETS_TABS.INVENTORY]: { label: "Inventaire", icon: Backpack },
-  [SHEETS_TABS.BIO]: { label: "Bio", icon: User },
-  [SHEETS_TABS.QUESTS]: { label: "Quêtes", icon: FileQuestionMark },
-  [SHEETS_TABS.SETTINGS]: { label: "Config", icon: Wrench },
-};
-
 export default async function Character({
   params,
+  searchParams,
 }: {
   params: Promise<{ id: string }>;
+  searchParams: Promise<{ tab: SHEETS_TABS }>;
 }) {
   const { id } = await params;
+  const awaitedSearchParams = await searchParams;
+  const currentTab = awaitedSearchParams.tab;
+
   const character = await getValidCharacter(id);
 
   const tabContentClassName = "mt-0 flex justify-center";
 
   return (
-    <Tabs defaultValue={SHEETS_TABS.GENERAL}>
-      <TabsList className="h-auto w-full flex-wrap">
-        {entries(tabs).map(([key, { label, icon: Icon }]) => (
-          <TabsTrigger key={key} value={key}>
-            <div className="flex items-center gap-1">
-              <Icon className="size-5" />
-              <span className="hidden md:block">{label}</span>
-            </div>
-          </TabsTrigger>
-        ))}
-      </TabsList>
+    <Tabs defaultValue={currentTab ?? SHEETS_TABS.GENERAL}>
+      <SheetTabTrigger />
 
       <Card
         className={`mt-4 border-background bg-background md:mb-0 md:ml-0 md:mr-0 md:border-border md:bg-card`}
