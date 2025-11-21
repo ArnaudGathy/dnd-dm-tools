f7a0308027718013ff1fb7c7aaf8963fba948ea9
Arnaud Gathy <arnaud.gathy@gmail.com>
2025-11-21 20:13:16 +0100

    Updated death tracker styles

--- src/app/(plain)/tracker/death/DeathTracker.tsx
+++ src/app/(plain)/tracker/death/DeathTracker.tsx
@@ -4,6 +4,7 @@ import { useDeathTracker } from "@/hooks/useDeathTracker";
 import Card from "@/app/(plain)/tracker/initiative/(initiative)/Card";
 import { Heart, Skull } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function DeathTracker() {
   const { deaths } = useDeathTracker();
@@ -13,33 +14,50 @@ export default function DeathTracker() {
   }
 
   return (
-    <div className="flex gap-4">
+    <div className="flex justify-center gap-4">
       {deaths.map(({ characterName, success, failure }) => (
-        <Card key={characterName}>
-          <div className="flex flex-col p-1">
-            <div className="mb-2 truncate text-center text-3xl font-bold">{characterName}</div>
-            <div className="flex gap-1">
-              {[...Array(3)].map((_, index) => (
-                <Heart
-                  key={index}
-                  className={cn("size-10 stroke-cyan-700 stroke-[2.5px]", {
-                    "stroke-stone-600/20": index >= success,
-                  })}
-                />
-              ))}
-            </div>
-            <div className="flex gap-1">
-              {[...Array(3)].map((_, index) => (
-                <Skull
-                  key={index}
-                  className={cn("size-10 stroke-red-700 stroke-[2.5px]", {
-                    "stroke-stone-600/20": index >= failure,
-                  })}
-                />
-              ))}
-            </div>
-          </div>
-        </Card>
+        <AnimatePresence mode="wait" key={characterName}>
+          <motion.div
+            key="loader"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 2 }}
+            className="flex"
+          >
+            <Card>
+              <div className="flex flex-col p-1">
+                <div className="mb-2 truncate text-center text-3xl font-bold">{characterName}</div>
+                <div className="flex gap-1">
+                  {[...Array(3)].map((_, index) => (
+                    <Heart
+                      key={index}
+                      className={cn(
+                        "size-10 stroke-cyan-700 stroke-[2.5px] transition duration-1000",
+                        {
+                          "stroke-stone-600/20": index >= success,
+                        },
+                      )}
+                    />
+                  ))}
+                </div>
+                <div className="flex gap-1">
+                  {[...Array(3)].map((_, index) => (
+                    <Skull
+                      key={index}
+                      className={cn(
+                        "size-10 stroke-red-700 stroke-[2.5px] transition duration-1000",
+                        {
+                          "stroke-stone-600/20": index >= failure,
+                        },
+                      )}
+                    />
+                  ))}
+                </div>
+              </div>
+            </Card>
+          </motion.div>
+        </AnimatePresence>
       ))}
     </div>
   );
